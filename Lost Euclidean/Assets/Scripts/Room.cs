896bc86c1eea43074c74fc9666c7c1e417872b5b
Ryan Zhang <zhangryan09@gmail.com>
2022-11-18 02:10:51 -0500

    Set up Room basic blockout.

---
+++ Lost Euclidean/Assets/Scripts/Room.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/**
+ * Class representing each individual room in the game.
+ */
+public class Room : MonoBehaviour
+{
+    /*
+     * Random Notes:
+     * Rooms are currently built to fit a 16:9 aspect ratio screen.
+     * Rooms including walls are 18 tiles by 11 tiles, the room floor then is 16 tiles by 9 tiles.
+     */
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
