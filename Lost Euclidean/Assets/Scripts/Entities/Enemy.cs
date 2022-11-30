777ec932f172d637f00bbeb171205b14dd67031a
shradercinc <82776089+shradercinc@users.noreply.github.com>
2022-11-30 01:44:49 -0500

    Enabled basic enemy code

--- Lost Euclidean/Assets/Scripts/Entities/Enemy.cs
+++ Lost Euclidean/Assets/Scripts/Entities/Enemy.cs
@@ -4,11 +4,18 @@ using UnityEngine;
 
 public class Enemy : MonoBehaviour
 {
+    public float speed = 3;
     public float health = 10;
+    public int damage = 1;
+    public Rigidbody rb;
+    public Transform pos;
+    private GameObject pl;
     // Start is called before the first frame update
     void Start()
     {
-
+        rb = GetComponent<Rigidbody>();
+        pos = GetComponent<Transform>();
+        pl = GameObject.FindGameObjectWithTag("Player");
     }
 
     private void OnTriggerEnter(Collider other)
@@ -23,6 +30,14 @@ public class Enemy : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+
+        Vector3 dir = (pl.GetComponent<Transform>().position - pos.position);
+        dir.y = 0;
+        dir.Normalize();
+        transform.rotation = Quaternion.LookRotation(dir);
+        rb.velocity = new Vector3(0, 0, 0);
+        rb.velocity += transform.forward * speed;
+
         if (health <= 0)
         {
             Destroy(this.gameObject);
